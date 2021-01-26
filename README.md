f789ed053d23ae19954d5b1441e209f791a0ded7
Kanae_Yamazaki <kanae.yamazaki@nijibox.com>
2021-01-26 16:44:28 +0900

    init Environment Environment init environment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# hello_project
+# hello-project
+
+## ハロプロ祭り
+
+- 初回
+
+```
+npm i
+```
+
+- サーバー立ち上げ http://localhost:8080/
+
+```
+npm run start
+```
+
+- ビルド
+
+```
+npm run build
+```
---
+++ src/client/App.tsx
@@ -0,0 +1,9 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+
+const app = (): JSX.Element => {
+	return <h2>Hello world.</h2>;
+};
+
+export default app;
