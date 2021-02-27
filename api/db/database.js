b49d87c2cf8171b5d2db66c0116c027dfb0e2348
Kanae.Yamazaki <kanae.jp81@gmail.com>
2021-02-27 21:08:05 +0900

    db作成

---
+++ api/db/database.js
@@ -0,0 +1,44 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('helloProject');
+
+const table = `
+    id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    kana TEXT NOT NULL,
+    nick_name TEXT,
+    age INTEGER,
+    birth_day TEXT,
+    birth_place TEXT,
+    group_name TEXT,
+    color TEXT,
+    date_of_join TEXT,
+    date_of_guraduation TEXT,
+    blog TEXT
+`
+const data = [{
+    name: '譜久村聖',
+    kana: 'フクムラ ミズキ',
+    nick_name: 'ふくちゃん',
+    age: 24,
+    birth_day:'1996/10/30',
+    birth_place:'東京都',
+    group_name:'モーニング娘。',
+    color:'ホットピンク',
+    date_of_join: '2011/01/02',
+    date_of_guraduation: '',
+    blog: 'https://ameblo.jp/morningmusume-9ki',
+}]
+
+
+// SQL 文を逐次処理する
+db.serialize();
+db.run(`DROP TABLE members`);
+db.run(`CREATE TABLE members (${table})`);
+
+// const sth = db.prepare("INSERT INTO members (name, kana, nick_name, age, birth_day, birth_place, group_name, color, date_of_join, date_of_guraduation, blog) VALUES (?,?,?,?,?,?,?,?,?,?.?)");
+for (let xs of data) {
+    db.run(`INSERT INTO members (name, kana, nick_name, age, birth_day, birth_place, group_name, color, date_of_join, date_of_guraduation, blog) VALUES ("${xs.name}","${xs.kana}","${xs.nick_name}","${xs.age}","${xs.birth_day}","${xs.birth_place}","${xs.group_name}","${xs.color}","${xs.date_of_join}","${xs.date_of_guraduation}","${xs.blog}")`,(err) => {
+        console.log(err)
+    });
+}
+db.close();
