4fb55774c5f2dcb0ab3aaf11c43e81423ce69dbf
Kanae_Yamazaki <kanae.yamazaki@nijibox.com>
2021-03-22 16:38:41 +0900

    install typescript-fsa

--- src/client/modules/actionTypes.ts
+++ src/client/modules/actionTypes.ts
@@ -1,19 +1,27 @@
-const TEST = "TEST";
-const FETCH_ALLDATA_START = "FETCH_ALLDATA_START";
-const FETCH_ALLDATA_SUCCEED = "FETCH_ALLDATA_SUCCEED";
-const FETCH_ALLDATA_FAIL = "FETCH_ALLDATA_FAIL";
+import actionCreateFactory from 'typescript-fsa';
+import { Member } from "../types";
 
-const SEARCH_NAME_START = "SEARCH_NAME_START";
-const SEARCH_NAME_SUCCEED = "SEARCH_NAME_SUCCEED";
-const SEARCH_NAME_FAIL = "SEARCH_NAME_FAIL";
+const actionCreator = actionCreateFactory();
+const TEST = actionCreator<string>("TEST");
 
-const SEARCH_DETAILS_START = "SEARCH_DETAILS_START";
-const SEARCH_DETAILS_SUCCEED = "SEARCH_DETAILS_SUCCEED";
-const SEARCH_DETAILS_FAIL = "SEARCH_DETAILS_FAIL";
 
-const SET_ERROR_MESSAGE = "SET_ERROR_MESSAGE";
+const FETCH_ALLDATA_START = actionCreator<void>("FETCH_ALLDATA_START");
+const FETCH_ALLDATA_SUCCEED = actionCreator<Member[]>("FETCH_ALLDATA_SUCCEED");
+const FETCH_ALLDATA_FAIL = actionCreator<string>("FETCH_ALLDATA_FAIL");
 
-const SET_SELECT_DATA = "SET_SELECT_DATA";
+const SEARCH_NAME_START = actionCreator<string>("SEARCH_NAME_START");
+const SEARCH_NAME_SUCCEED = actionCreator<Member[]>("SEARCH_NAME_SUCCEED");
+const SEARCH_NAME_FAIL = actionCreator<string>("SEARCH_NAME_FAIL");
+
+const SEARCH_DETAILS_START = actionCreator<void>("SEARCH_DETAILS_START");
+const SEARCH_DETAILS_SUCCEED = actionCreator<Member[]>("SEARCH_DETAILS_SUCCEED");
+const SEARCH_DETAILS_FAIL = actionCreator<string>("SEARCH_DETAILS_FAIL");
+
+const SET_ERROR_MESSAGE = actionCreator<string>("SET_ERROR_MESSAGE");
+
+const SET_SELECT_DATA = actionCreator<{ id: string; value: string }>(
+  "SET_SELECT_DATA"
+);
 
 export default {
   TEST,
--- src/client/modules/reducers.ts
+++ src/client/modules/reducers.ts
@@ -1,5 +1,7 @@
 import types from "./actionTypes";
 import { Member } from "../types/index";
+import { Action } from "redux";
+import { isType } from "typescript-fsa";
 
 export type Search = {
   name: string;
@@ -17,26 +19,6 @@ export type InitialState = {
   error: string;
 };
 
-// type ActionType = {
-//         type: 'FETCH_ALLDATA_START' | 'SEARCH_DETAILS_START';
-//     } | {
-//         type: 'SET_SELECT_DATA';
-//         payload: {
-//             id: string;
-//             value: string;
-//         };
-//     } | {
-//         type: types.TEST | 'SET_ERROR_MESSAGE' | 'FETCH_ALLDATA_FAIL' | 'SEARCH_NAME_START' | 'SEARCH_NAME_FAIL' | 'SEARCH_DETAILS_FAIL';
-//         payload: string;
-//     } | {
-//         type: 'FETCH_ALLDATA_SUCCEED' | 'SEARCH_NAME_SUCCEED' | 'SEARCH_DETAILS_SUCCEED';
-//         payload: Member[];
-//     };
-type ActionType = {
-  type: string;
-  payload?: string | Member[] | { id: string; value: string };
-};
-
 const initialDateState: InitialState = {
   test: "",
   isLoading: false,
@@ -51,117 +33,95 @@ const initialDateState: InitialState = {
   error: "",
 };
 
-const reducer = (state = initialDateState, action: ActionType) => {
-  switch (action.type) {
-	case types.TEST:
-	  const test = action.payload as string;
-	  return {
-		...state,
-		test: test,
-	  };
-	case types.FETCH_ALLDATA_START:
-	  return {
-		...state,
-		isLoading: true,
-	  };
-	case types.FETCH_ALLDATA_SUCCEED:
-	  const allMembers = action.payload as Member[];
-	  return {
-		...state,
-		isLoading: false,
-		members: allMembers,
-		error: "",
-	  };
-	case types.FETCH_ALLDATA_FAIL:
-	  const allMembersErr = action.payload as string;
-	  return {
-		...state,
-		isLoading: false,
-		members: [],
-		error: allMembersErr,
-	  };
-	case types.SEARCH_NAME_START:
-	  const name = action.payload as string;
-	  return {
-		...state,
-		isLoading: true,
-		search: {
-		  ...state.search,
-		  name: name,
-		},
-	  };
-	case types.SEARCH_NAME_SUCCEED:
-	  const searchNameMembers = action.payload as Member[];
-	  return {
-		...state,
-		isLoading: false,
-		members: searchNameMembers,
-		error: "",
-	  };
-	case types.SEARCH_NAME_FAIL:
-	  const searchNameMembersErr = action.payload as string;
-	  return {
-		...state,
-		isLoading: false,
-		members: [],
-		error: searchNameMembersErr,
-	  };
-	case types.SEARCH_DETAILS_START:
-	  return {
-		...state,
-		isLoading: true,
-	  };
-	case types.SEARCH_DETAILS_SUCCEED:
-	  const searchDetailMembers = action.payload as Member[];
-	  return {
-		...state,
-		isLoading: false,
-		members: searchDetailMembers,
-		error: "",
-	  };
-	case types.SEARCH_DETAILS_FAIL:
-	  const searchDetailMembersErr = action.payload as string;
-	  return {
-		...state,
-		isLoading: false,
-		members: [],
-		error: searchDetailMembersErr,
-	  };
-	case types.SET_SELECT_DATA:
-	  const id =
-		!action.payload ||
-		typeof action.payload === "string" ||
-		Array.isArray(action.payload)
-		  ? ""
-		  : action.payload.id;
-	  const value =
-		!action.payload ||
-		typeof action.payload === "string" ||
-		Array.isArray(action.payload)
-		  ? ""
-		  : action.payload.value;
-	  return {
-		...state,
-		search: {
-		  ...state.search,
-		  select: {
-			...state.search.select,
-			[id]: value,
-		  },
-		},
-	  };
-	case types.SET_ERROR_MESSAGE:
-	  const err = action.payload as string;
-	  return {
-		...state,
-		isLoading: false,
-		error: err,
-	  };
-	default:
-	  return {
-		...state,
-	  };
-  }
+const reducer = (state = initialDateState, action: Action) => {
+    if (isType(action, types.TEST)) {
+        return {
+            ...state,
+            test: action.payload,
+        }
+    }  else if (isType(action, types.FETCH_ALLDATA_START)) {
+        return {
+            ...state,
+            isLoading: true,
+        };
+    } else if (isType(action, types.FETCH_ALLDATA_SUCCEED)) {
+        return {
+            ...state,
+            isLoading: false,
+            members: action.payload,
+            error: "",
+        };
+    } else if (isType(action, types.FETCH_ALLDATA_FAIL)) {
+        return {
+            ...state,
+            isLoading: false,
+            members: [],
+            error: action.payload,
+        };
+    } else if (isType(action, types.SEARCH_NAME_START)) {
+        return {
+            ...state,
+            isLoading: true,
+            search: {
+            ...state.search,
+            name: action.payload,
+            },
+        };
+    } else if (isType(action, types.SEARCH_NAME_SUCCEED)) {
+        return {
+            ...state,
+            isLoading: false,
+            members: action.payload,
+            error: "",
+        };
+    } else if (isType(action, types.SEARCH_NAME_FAIL)) {
+        return {
+            ...state,
+            isLoading: false,
+            members: [],
+            error: action.payload,
+        };
+    } else if (isType(action, types.SEARCH_DETAILS_START)){
+        return {
+            ...state,
+            isLoading: true,
+        };
+    } else if (isType(action, types.SEARCH_DETAILS_SUCCEED)){
+        return {
+            ...state,
+            isLoading: false,
+            members: action.payload,
+            error: "",
+        };
+    } else if (isType(action, types.SEARCH_DETAILS_FAIL)){
+        return {
+            ...state,
+            isLoading: false,
+            members: [],
+            error: action.payload,
+        };
+    } else if (isType(action, types.SET_SELECT_DATA)){
+        return {
+            ...state,
+            search: {
+            ...state.search,
+            select: {
+                ...state.search.select,
+                [action.payload.id]: action.payload.value,
+            },
+            },
+        };
+    } else if (isType(action, types.SET_ERROR_MESSAGE)){
+        return {
+            ...state,
+            isLoading: false,
+            error: action.payload,
+        };
+    } else {
+        return {
+            ...state,
+        };
+    }
 };
 
 export default reducer;
